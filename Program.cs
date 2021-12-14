b7b73c0898b1ba282e04609c72d417a2549e6be8
enes.nayci <enes.nayci@hotmail.com>
2021-12-14 23:45:56 +0300

    Added --> template of project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Net;
+
+public class Program
+{
+	public static Task Main(string[] args) => new Program().MainAsync();
+
+	public async Task MainAsync()
+	{
+	}
+}
