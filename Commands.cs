3ae4afb6109dceac01122a2ad38ff4443bb6e03a
Enes Naycı <enes.nayci@hotmail.com>
2021-12-17 00:12:20 -0800

    Revert "Revert "Delete""
    This reverts commit 87016c7d96925154ee3883adfc627badffff3d1f.

--- Commands.cs
+++ Commands.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Discord.Commands;
 
-public class selam : ModuleBase<SocketCommandContext>
+public class General : ModuleBase<SocketCommandContext>
 {
     [Command("selam")]
     [Summary("Selamla")]
@@ -10,5 +10,15 @@ public class selam : ModuleBase<SocketCommandContext>
     public async Task selamBackAsync(){
         System.Console.WriteLine("Selam --used");
         await Context.Channel.SendMessageAsync("Selam dostum :)");
+    }
+}
+public class GG : ModuleBase<SocketCommandContext>
+{
+    [Command("GG")]
+    [Summary("GoodGame")]
+
+    public async Task GgBackAsync(ulong id){
+        
+        await Context.Channel.DeleteMessageAsync(id);
     }
 }
